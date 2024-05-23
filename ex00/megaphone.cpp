f2ac71a844e5c904ec5476d9d8cb2fef27072a11
Mohamad <zolfagharipour@gmail.com>
2024-05-23 09:09:00 +0200

    changed megaphone to cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,11 +6,12 @@
 /*   By: mzolfagh <mzolfagh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 11:51:35 by mzolfagh          #+#    #+#             */
-/*   Updated: 2024/04/22 13:19:02 by mzolfagh         ###   ########.fr       */
+/*   Updated: 2024/05/23 09:08:21 by mzolfagh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
 int main(int ac, char **av)
 {
@@ -18,14 +19,27 @@ int main(int ac, char **av)
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
     else
     {
-        for (int i = 1; av[i]; i++)
+        // for (int i = 1; av[i]; i++)
+        // {
+        //     for (int j = 0; av[i][j]; j++)
+        //     {
+        //         std::cout << (char)toupper(av[i][j]);
+        //     }
+        //     std::cout << ' ';
+        // }
+        // std::cout << std::endl;
+        
+        for (int i = 1; i < ac; i++)
         {
-            for (int j = 0; av[i][j]; j++)
+            std::string arg(av[i]);
+                        
+            for (int j = 0; j < arg.length(); j++)
             {
-                std::cout << (char)toupper(av[i][j]);
+                arg[j] =  std::toupper (static_cast<unsigned char> (arg[j]));
             }
-            std::cout << ' ';
+            std::cout << arg << ' ';
         }
         std::cout << std::endl;
+        
     }
 }
